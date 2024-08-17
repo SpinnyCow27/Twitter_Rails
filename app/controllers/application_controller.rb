94a8f2cabee02d13387e5dc33b1c2b3d4aba5e0d
SpinnyCow27 <matias.04@hotmail.es>
2024-08-17 13:32:38 -0400

    Seteado pagy, un controlador y la ruta correctamente

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  include Pagy::Backend
   Pagy::DEFAULT[:limit] = 10
+  include Pagy::Backend
 end
